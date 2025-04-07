082234506410c97f122b00072578ebccfc8dafb9
Ciro Rodriguez <cirorod0103@gmail.com>
2025-04-07 20:31:23 -0300

    se creo el modelo categoria

--- src/producto/models.py
+++ src/producto/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=50, unique=True)
+
+    def __str__(self) -> str:
+        return self.nombre
 # Create your models here.

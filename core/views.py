61743e28404f913faaeab0f9a93095da6a42c727
Ciro Rodriguez <cirorod0103@gmail.com>
2025-04-06 23:20:50 -0300

    Se configuro core

---
+++ core/views.py
@@ -0,0 +1,42 @@
+
+from django.contrib import messages
+
+# from .models import Cliente
+from django.contrib.auth.decorators import login_not_required # type: ignore
+from django.contrib.auth.views import LoginView
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from .forms import LoginForm, RegisterForm
+
+
+@login_not_required
+def index(request):
+    return render(request, "core/index.html")
+
+
+@login_not_required
+def about(request):
+    return render(request, "core/about.html")
+
+
+class MiLoginView(LoginView):
+    template_name = "core/login.html"
+    authentication_form = LoginForm
+    next_page = reverse_lazy("core:index")
+
+    def form_valid(self, form):
+        usuario = form.get_user()
+        messages.success(self.request, f"Inicio de sesión exitoso. ¡Bienvenido {usuario.username}!")
+        return super().form_valid(form)
+
+
+class MiRegisterView(CreateView):
+    form_class = RegisterForm
+    template_name = "core/register.html"
+    success_url = reverse_lazy("core:login")
+
+    def form_valid(self, form):
+        messages.success(self.request, f"Registro exitoso. Ahora puedes iniciar sesión")
+        return super().form_valid(form)

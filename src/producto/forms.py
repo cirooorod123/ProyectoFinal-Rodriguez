ae79d0ac6ca53be5ec3fd387cdc78a1873f73340
Ciro Rodriguez <cirorod0103@gmail.com>
2025-04-07 20:38:59 -0300

    se crearon los formularios

---
+++ src/producto/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+
+from . import models
+
+
+class CategoriaForm(forms.ModelForm):
+    class Meta:
+        model = models.Categoria
+        fields = "__all__"
+
+
+class ProductoForm(forms.ModelForm):
+    class Meta:
+        model = models.Producto
+        fields = "__all__"
+

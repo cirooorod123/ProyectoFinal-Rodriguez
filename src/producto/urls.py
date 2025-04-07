b1fc1768261565e1596d4e779ad37edd5fa901fd
Ciro Rodriguez <cirorod0103@gmail.com>
2025-04-07 20:44:08 -0300

    se crearon las urls

---
+++ src/producto/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+
+from . import views
+
+app_name = "producto"
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("categoria/list/", views.categoria_list, name="categoria_list"),
+    path("categoria/create/", views.categoria_create, name="categoria_create"),
+    path("categoria/update/<int:pk>", views.categoria_update, name="categoria_update"),
+    path("categoria/delete/<int:pk>", views.categoria_delete, name="categoria_delete"),
+]
+
+urlpatterns += [
+    path("producto/list/", views.ProductoListView.as_view(), name="producto_list"),
+    path("producto/create/", views.ProductoCreateView.as_view(), name="producto_create"),
+    path("producto/update/<int:pk>", views.ProductoUpdateView.as_view(), name="producto_update"),
+    path("producto/detail/<int:pk>", views.ProductoDetailView.as_view(), name="producto_detail"),
+    path("producto/delete/<int:pk>", views.ProductoDeleteView.as_view(), name="producto_delete"),
+]

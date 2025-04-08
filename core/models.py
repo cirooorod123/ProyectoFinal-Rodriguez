f2d60b580307647f982c72a15900c62001db7597
cirooorod123 <cirorod0103@gmail.com>
2025-04-07 21:11:59 -0300

    Delete core directory

--- core/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Cliente(models.Model):
-    nombre = models.CharField(max_length=50)
-    apellido = models.CharField(max_length=50)
-
-    def __str__(self) -> str:
-        return f"{self.apellido}, {self.nombre}"

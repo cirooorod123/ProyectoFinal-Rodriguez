94297202bbe9363bc00e98130ab65c4b1230c3d2
Ciro Rodriguez <cirorod0103@gmail.com>
2025-04-07 22:58:06 -0300

    se configuro la aplicacion cliente

--- src/cliente/admin.py
+++ src/cliente/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+admin.site.register(models.Cliente)
+
+@admin.register(models.Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "apellido", "email", "telefono")
+    search_fields = ("nombre", "apellido")
+    list_filter = ("barrio",)
+    ordering = ("apellido", "nombre")
+    date_hierarchy = "nacimiento"
--- src/cliente/models.py
+++ src/cliente/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+ 
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    email = models.EmailField()
+    telefono = models.CharField(max_length=15, blank=True, null=True)
+    nacimiento = models.DateField()
+    barrio = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
 
 # Create your models here.
